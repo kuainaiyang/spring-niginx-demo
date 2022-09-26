1196a8c9435bca19498484b15089be7aad5decd9
“loki”  <731488890@qq.com>
2022-09-26 17:35:05 +0800

    nginx 简单配置
    (cherry picked from commit b920625bab7d698a27e54dc0046902d763bd9dea)

---
+++ src/test/java/com/lingdong/nginx/NginxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingdong.nginx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
